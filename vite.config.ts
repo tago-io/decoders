f619c85b5593b5d6f04388fd0f7fc23b7e7c3fcc
Mateus Silva <mateus.silva@tago.io>
2024-05-24 15:21:59 -0300

    Configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vitest/config'
+import swc from "unplugin-swc";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: ['**/node_modules/**'],
+    globals: true,
+    root: "./decoders",
+    exclude: ["**/node_modules/**"],
   },
-})
+  plugins: [
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: "es6" },
+    }),
+  ],
+});
