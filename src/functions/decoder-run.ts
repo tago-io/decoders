8e796563dd2e59a32e952d02279c1966a3b93ddd
Mateus Silva <mateus.silva@tago.io>
2024-05-31 18:00:05 -0300

    chore: Update npm dependencies and add unplugin-swc for faster compilation

--- src/functions/decoder-run.ts
+++ src/functions/decoder-run.ts
@@ -11,7 +11,7 @@ import moment from "moment-timezone";
 import { buildTS } from "../helpers/build-ts";
 
 function decoderRun(file_path: string, params = {}) {
-  let file = fs.readFileSync(path.join(__dirname, "../", file_path), "utf-8");
+  let file = fs.readFileSync(path.join(__dirname, "../../", file_path), "utf-8");
 
   file = buildTS(file);
 
@@ -19,8 +19,8 @@ function decoderRun(file_path: string, params = {}) {
   vm.freeze(moment, "moment");
   const footer = "\n ; this.payload";
   return vm.run(`${file}${footer}`);
-};
+}
 
 // ? It is necessary to export in this way to be used in the tests
 module.exports = decoderRun; // ? CommonJS
-export {decoderRun }; // ? ESM
+export { decoderRun }; // ? ESM
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vitest/config'
+import swc from "unplugin-swc";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: ['**/node_modules/**'],
+    globals: true,
+    exclude: ["**/node_modules/**"],
   },
-})
+  plugins: [
+    swc.vite({
+      module: { type: "es6" },
+    }),
+  ],
+});
