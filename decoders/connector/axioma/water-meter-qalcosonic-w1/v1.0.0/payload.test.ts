c2bf72ae0fac29cdfb2da4378e9ee0464e237804
Frederico Minuzzi <freddyminu@gmail.com>
2024-08-27 15:18:50 -0300

    Removed necessity of port numbers, the payload type is now done via the size of the payload. Port 100 is always larger than 5 bytes while 103 is always 5 bytes. Added Unit tests and Typescript.

---
+++ decoders/connector/axioma/water-meter-qalcosonic-w1/v1.0.0/payload.test.ts
@@ -0,0 +1,111 @@
+import { describe, expect, test } from "vitest";
+
+import { decoderRun } from "../../../../../src/functions/decoder-run";
+
+const file_path = "decoders/connector/axioma/water-meter-qalcosonic-w1/v1.0.0/payload.ts" as const;
+
+function preparePayload(payloadHex: string) {
+  let payload = [{ variable: "payload", value: payloadHex }];
+  payload = decoderRun(file_path, { payload });
+
+  const status_code = payload.find((item) => item.variable === "status_code");
+  const current_volume = payload.find((item) => item.variable === "current_volume");
+  const first_log_datetime = payload.find((item) => item.variable === "first_log_datetime");
+  const volume_at_log_datetime = payload.find((item) => item.variable === "volume_at_log_datetime");
+
+  const log_volume_delta_1 = payload.find((item) => item.variable === "log_volume_delta_1");
+  const log_volume_delta_2 = payload.find((item) => item.variable === "log_volume_delta_2");
+  const log_volume_delta_3 = payload.find((item) => item.variable === "log_volume_delta_3");
+  const log_volume_delta_4 = payload.find((item) => item.variable === "log_volume_delta_4");
+  const log_volume_delta_5 = payload.find((item) => item.variable === "log_volume_delta_5");
+  const log_volume_delta_6 = payload.find((item) => item.variable === "log_volume_delta_6");
+  const log_volume_delta_7 = payload.find((item) => item.variable === "log_volume_delta_7");
+  const log_volume_delta_8 = payload.find((item) => item.variable === "log_volume_delta_8");
+  const log_volume_delta_9 = payload.find((item) => item.variable === "log_volume_delta_9");
+  const log_volume_delta_10 = payload.find((item) => item.variable === "log_volume_delta_10");
+  const log_volume_delta_11 = payload.find((item) => item.variable === "log_volume_delta_11");
+  const log_volume_delta_12 = payload.find((item) => item.variable === "log_volume_delta_12");
+  const log_volume_delta_13 = payload.find((item) => item.variable === "log_volume_delta_13");
+  const log_volume_delta_14 = payload.find((item) => item.variable === "log_volume_delta_14");
+  const log_volume_delta_15 = payload.find((item) => item.variable === "log_volume_delta_15");
+
+  const parse_error = payload.find((item) => item.variable === "parse_error");
+  return {
+    payload,
+    status_code,
+    current_volume,
+    first_log_datetime,
+    volume_at_log_datetime,
+    log_volume_delta_1,
+    log_volume_delta_2,
+    log_volume_delta_3,
+    log_volume_delta_4,
+    log_volume_delta_5,
+    log_volume_delta_6,
+    log_volume_delta_7,
+    log_volume_delta_8,
+    log_volume_delta_9,
+    log_volume_delta_10,
+    log_volume_delta_11,
+    log_volume_delta_12,
+    log_volume_delta_13,
+    log_volume_delta_14,
+    log_volume_delta_15,
+
+    // parse error
+    parse_error,
+  };
+}
+
+describe("Port 100 unit tests", () => {
+  const payloadHex = "0ea0355d302935000030b6345de7290000b800b900b800b800b800b900b800b800b800b800b800b800b900b900b900";
+  const result = preparePayload(payloadHex);
+
+  test("Output Result", () => {
+    expect(Array.isArray(result.payload)).toBe(true);
+  });
+
+  test("Port 100 variable values", () => {
+    expect(result.status_code?.value).toBe(30);
+    expect(result.current_volume?.value).toBe(13.609);
+    expect(result.first_log_datetime?.value).toBe("2019-07-21T19:00:00.000Z");
+    expect(result.volume_at_log_datetime?.value).toBe(10.727);
+    expect(result.log_volume_delta_1?.value).toBe(0.184);
+    expect(result.log_volume_delta_1?.value).toBe(0.184);
+    expect(result.log_volume_delta_2?.value).toBe(0.185);
+    expect(result.log_volume_delta_3?.value).toBe(0.184);
+    expect(result.log_volume_delta_4?.value).toBe(0.184);
+    expect(result.log_volume_delta_5?.value).toBe(0.184);
+    expect(result.log_volume_delta_6?.value).toBe(0.185);
+    expect(result.log_volume_delta_7?.value).toBe(0.184);
+    expect(result.log_volume_delta_8?.value).toBe(0.184);
+    expect(result.log_volume_delta_9?.value).toBe(0.184);
+    expect(result.log_volume_delta_10?.value).toBe(0.184);
+    expect(result.log_volume_delta_11?.value).toBe(0.184);
+    expect(result.log_volume_delta_12?.value).toBe(0.184);
+    expect(result.log_volume_delta_13?.value).toBe(0.185);
+    expect(result.log_volume_delta_14?.value).toBe(0.185);
+    expect(result.log_volume_delta_15?.value).toBe(0.185);
+  });
+});
+
+describe("Port 103 unit tests", () => {
+  const payloadHex = "43b1315d30";
+  const result = preparePayload(payloadHex);
+
+  test("Port 103 variable values", () => {
+    expect(result.status_code?.value).toBe(30);
+  });
+});
+
+describe("Shall not be parsed", () => {
+  let payload = [{ variable: "shallnotpass", value: "04096113950292" }];
+  payload = decoderRun(file_path, { payload });
+  test("Output Result", () => {
+    expect(Array.isArray(payload)).toBe(true);
+  });
+
+  test("Not parsed Result", () => {
+    expect(payload).toEqual([{ variable: "shallnotpass", value: "04096113950292" }]);
+  });
+});
---
+++ decoders/connector/axioma/water-meter-qalcosonic-w1/v1.0.0/payload.ts
@@ -0,0 +1,108 @@
+/**
+ * This is the main function to parse the payload. Everything else doesn't require your attention.
+ * @param {String} payload_raw
+ * @returns {Object} containing key and value to TagoIO
+ */
+function toTagoFormat(object_item: any, group: string, prefix = "") {
+  const result: any = [];
+  for (const key in object_item) {
+    if (typeof object_item[key] === "object") {
+      result.push({
+        variable: object_item[key].variable || `${prefix}${key}`.toLowerCase(),
+        value: object_item[key].value,
+        group: object_item[key].group || group,
+        metadata: object_item[key].metadata,
+        location: object_item[key].location,
+        unit: object_item[key].unit,
+        time: object_item[key].time,
+      });
+    } else {
+      result.push({
+        variable: `${prefix}${key}`.toLowerCase(),
+        value: object_item[key],
+        group,
+      });
+    }
+  }
+
+  return result;
+}
+
+function parsePayload(payload_raw: string) {
+  try {
+    const bytes = Buffer.from(payload_raw, "hex");
+    const data: any = {};
+    const payloadLength = bytes.length;
+
+    // port 100
+    if (payloadLength > 5) {
+      let time = (bytes[3] << 24) | (bytes[2] << 16) | (bytes[1] << 8) | bytes[0];
+      const date = new Date(time * 1000);
+
+      const status_code = Number(payload_raw.substr(8, 2));
+      data.status_code = { value: status_code, date };
+
+      const current_volume = bytes.readUInt32LE(5) * 0.001;
+      data.current_volume = { value: current_volume, unit: "m3", date };
+
+      const first_log_datetime = new Date(((bytes[12] << 24) | (bytes[11] << 16) | (bytes[10] << 8) | bytes[9]) * 1000).toISOString();
+      data.first_log_datetime = {
+        value: first_log_datetime,
+        time: first_log_datetime,
+      };
+
+      const volume_at_log_datetime = bytes.readUInt32LE(13) * 0.001;
+      data.volume_at_log_datetime = {
+        value: volume_at_log_datetime,
+        unit: "m3",
+        time: first_log_datetime,
+      };
+
+      for (let i = 0; i < 15; i++) {
+        const log_volume_delta = bytes.readUInt16LE(17 + i * 2) * 0.001;
+        data[`log_volume_delta_${i + 1}`] = {
+          value: log_volume_delta,
+          unit: "m3",
+          time: new Date(new Date(first_log_datetime).getTime() + (i + 1) * 3600000),
+        };
+      }
+
+      return data;
+    }
+
+    // port 103
+    if (payloadLength === 5) {
+      let time = (bytes[3] << 24) | (bytes[2] << 16) | (bytes[1] << 8) | bytes[0];
+      const date = new Date(time * 1000);
+
+      const status_code = Number(payload_raw.substr(8, 2));
+      data.status_code = { value: status_code, date };
+
+      return data;
+    }
+
+    return data;
+  } catch (e) {
+    console.log(e);
+    // Return the variable parse_error for debugging.
+    return [{ variable: "parse_error", value: e.message }];
+  }
+}
+
+// Payload is an environment variable. Is where what is being inserted to your device comes in.
+// Payload always is an array of objects. [ { variable, value...}, {variable, value...} ...]
+const axioma_payload = payload.find((x) => x.variable === "payload_raw" || x.variable === "payload" || x.variable === "data");
+
+if (axioma_payload) {
+  // Get a unique group for the incoming data.
+  const { value } = axioma_payload;
+  let { group } = axioma_payload;
+  group = String(new Date().getTime());
+
+  // Parse the payload_raw to JSON format (it comes in a String format)
+  if (value) {
+    payload = payload.concat(toTagoFormat(parsePayload(value.replace(/ /g, "")), group));
+  }
+}
+
+// console.log(JSON.stringify(payload));
