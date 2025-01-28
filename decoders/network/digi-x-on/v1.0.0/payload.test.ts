1580adbe2dd58fd010d07bf7ee58f6f3cf8194fa
Mateus Silva <mateus.silva@tago.io>
2025-01-28 15:55:26 -0300

    Add tests for Digi X-ON payload decoder and update parsing logic

---
+++ decoders/network/digi-x-on/v1.0.0/payload.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test, beforeEach } from "vitest";
+import { DataToSend } from "@tago-io/sdk/lib/types";
+
+import { decoderRun } from "../../../../src/functions/decoder-run";
+
+const file_path = "decoders/network/digi-x-on/v1.0.0/payload.ts";
+
+let payload: DataToSend[] = [];
+
+describe("Shall not be parsed", () => {
+  beforeEach(() => {
+    payload = [{ variable: "shallnotpass", value: "04096113950292" }];
+  });
+  payload = decoderRun(file_path, { payload });
+  test("Output Result", () => {
+    expect(Array.isArray(payload)).toBe(true);
+  });
+  test("Not parsed Result", () => {
+    expect(payload).toEqual([{ variable: "shallnotpass", value: "04096113950292" }]);
+  });
+});
