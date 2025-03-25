d87f99171c523920e7a1eb333b06106f5c54f4da
Mateus Silva <mateus.silva@tago.io>
2025-03-25 15:02:52 -0300

    Update IMEI and IMSI values in Dragino SN50v3-CB payload tests

--- decoders/connector/dragino/sn50v3-cb/v1.0.0/payload.test.ts
+++ decoders/connector/dragino/sn50v3-cb/v1.0.0/payload.test.ts
@@ -15,8 +15,8 @@ describe("Dragino SN50v3-CB Payload Decoder for MQTT", () => {
       "6": [0, 0, 1, -409.5, "2000/01/01 05:59:11"],
       "7": [0, 0, 1, -409.5, "2000/01/01 05:44:11"],
       "8": [0, 0, 1, -409.5, "2000/01/01 05:29:11"],
-      IMEI: "861562073745280",
-      IMSI: "724068042540503",
+      IMEI: "111222333444555",
+      IMSI: "123456789012345",
       Model: "SN50V3-NB",
       mod: 1,
       battery: 3.59,
@@ -41,8 +41,8 @@ describe("Dragino SN50v3-CB Payload Decoder for MQTT", () => {
   test("Output Result", () => {
     expect(payload).toEqual(
       expect.arrayContaining([
-        expect.objectContaining({ variable: "imei", value: "861562073745280" }),
-        expect.objectContaining({ variable: "imsi", value: "724068042540503" }),
+        expect.objectContaining({ variable: "imei", value: "111222333444555" }),
+        expect.objectContaining({ variable: "imsi", value: "123456789012345" }),
         expect.objectContaining({ variable: "model", value: "SN50V3-NB" }),
         expect.objectContaining({ variable: "mod", value: 1 }),
         expect.objectContaining({ variable: "battery", value: 3.59, unit: "mV" }),
@@ -74,7 +74,7 @@ describe("Dragino SN50v3-CB Payload Decoding for JSON", () => {
     {
       variable: "payload_raw",
       value:
-        '{"IMEI": "860751074067197","IMSI": "724068042540503","Model": "DTN-300-FSN7-DC","mod": 8,"battery": 3.60,"signal": 24,"time": "2025/03/15 16:55:01","adc2": 2,"DS18B20_Temp": -409.5,"interrupt": 0,"interrupt_level": 0,"interrupt_pa4": 0,"interrupt_level_pa4": 0,"interrupt_pa8": 0,"interrupt_level_pa8": 0}',
+        '{"IMEI": "222333444555666","IMSI": "234567890123456","Model": "DTN-300-FSN7-DC","mod": 8,"battery": 3.60,"signal": 24,"time": "2025/03/15 16:55:01","adc2": 2,"DS18B20_Temp": -409.5,"interrupt": 0,"interrupt_level": 0,"interrupt_pa4": 0,"interrupt_level_pa4": 0,"interrupt_pa8": 0,"interrupt_level_pa8": 0}',
     },
   ];
   payload = decoderRun(file_path, { payload });
@@ -82,8 +82,8 @@ describe("Dragino SN50v3-CB Payload Decoding for JSON", () => {
   test("Output Result", () => {
     expect(payload).toEqual(
       expect.arrayContaining([
-        expect.objectContaining({ variable: "imei", value: "860751074067197" }),
-        expect.objectContaining({ variable: "imsi", value: "724068042540503" }),
+        expect.objectContaining({ variable: "imei", value: "222333444555666" }),
+        expect.objectContaining({ variable: "imsi", value: "234567890123456" }),
         expect.objectContaining({ variable: "model", value: "DTN-300-FSN7-DC" }),
         expect.objectContaining({ variable: "mod", value: 8 }),
         expect.objectContaining({ variable: "battery", value: 3.6, unit: "mV" }),
@@ -107,7 +107,7 @@ describe("Dragino SN50v3-CB Payload Decoding for complex JSON with historical da
     {
       variable: "payload_raw",
       value:
-        '{"IMEI":"868508065628110","IMSI":"460240210507481","Model":"SN50V3-CB","mod":1,"battery":3.21,"signal":20,"time":"2024-12-09T09:35:21Z","latitude":0.000000,"longitude":0.000000,"gps_time":"1970-01-01T00:00:00Z","DS18B20_Temp":25.6,"digital_in":0,"interrupt":0,"interrupt_level":0,"adc1":0,"temperature":25.5,"humidity":51.8,"1":[24.4,52.3,0,25.0,"2024-12-09T09:17:50Z"],"2":[24.0,52.5,0,25.5,"2024-12-09T09:02:50Z"],"3":[23.8,53.1,1,25.8,"2024-12-09T08:47:50Z"],"4":[204.8,1.0,0,0.4,"2024-12-09T07:47:05Z"],"5":[204.8,1.0,0,0.4,"2024-12-09T07:32:05Z"],"6":[23.3,54.2,0,0.4,"2024-12-09T06:09:17Z"],"7":[23.1,53.9,0,0.4,"2024-12-09T05:54:17Z"],"8":[22.5,54.8,0,0.4,"2024-12-09T05:39:17Z"]}',
+        '{"IMEI":"333444555666777","IMSI":"345678901234567","Model":"SN50V3-CB","mod":1,"battery":3.21,"signal":20,"time":"2024-12-09T09:35:21Z","latitude":0.000000,"longitude":0.000000,"gps_time":"1970-01-01T00:00:00Z","DS18B20_Temp":25.6,"digital_in":0,"interrupt":0,"interrupt_level":0,"adc1":0,"temperature":25.5,"humidity":51.8,"1":[24.4,52.3,0,25.0,"2024-12-09T09:17:50Z"],"2":[24.0,52.5,0,25.5,"2024-12-09T09:02:50Z"],"3":[23.8,53.1,1,25.8,"2024-12-09T08:47:50Z"],"4":[204.8,1.0,0,0.4,"2024-12-09T07:47:05Z"],"5":[204.8,1.0,0,0.4,"2024-12-09T07:32:05Z"],"6":[23.3,54.2,0,0.4,"2024-12-09T06:09:17Z"],"7":[23.1,53.9,0,0.4,"2024-12-09T05:54:17Z"],"8":[22.5,54.8,0,0.4,"2024-12-09T05:39:17Z"]}',
     },
   ];
   payload = decoderRun(file_path, { payload });
@@ -115,8 +115,8 @@ describe("Dragino SN50v3-CB Payload Decoding for complex JSON with historical da
   test("Output Result", () => {
     expect(payload).toEqual(
       expect.arrayContaining([
-        expect.objectContaining({ variable: "imei", value: "868508065628110" }),
-        expect.objectContaining({ variable: "imsi", value: "460240210507481" }),
+        expect.objectContaining({ variable: "imei", value: "333444555666777" }),
+        expect.objectContaining({ variable: "imsi", value: "345678901234567" }),
         expect.objectContaining({ variable: "model", value: "SN50V3-CB" }),
         expect.objectContaining({ variable: "mod", value: 1 }),
         expect.objectContaining({ variable: "battery", value: 3.21, unit: "mV" }),
@@ -149,7 +149,7 @@ describe("Dragino SN50v3-CB Payload Decoding for Hexadecimal", () => {
   let payload = [
     {
       variable: "payload_raw",
-      value: "f868508065628110f4602402105074814c6e0cba16010104000000000100fe01fd6756bb09000000000000000000000000",
+      value: "f444555666777888f4567890123456784c6e0cba16010104000000000100fe01fd6756bb09000000000000000000000000",
     },
   ];
 
@@ -158,8 +158,8 @@ describe("Dragino SN50v3-CB Payload Decoding for Hexadecimal", () => {
   test("Output Result", () => {
     expect(payload).toEqual(
       expect.arrayContaining([
-        expect.objectContaining({ variable: "device_id", value: "868508065628110" }),
-        expect.objectContaining({ variable: "sim_card_id", value: "460240210507481" }),
+        expect.objectContaining({ variable: "device_id", value: "444555666777888" }),
+        expect.objectContaining({ variable: "sim_card_id", value: "567890123456789" }),
         expect.objectContaining({ variable: "version", value: "SN50v3-CB, 76=1.1.0" }),
         expect.objectContaining({ variable: "battery", value: 3.258, unit: "V" }),
         expect.objectContaining({ variable: "signal", value: 22 }),
@@ -184,7 +184,7 @@ describe("Dragino SN50v3-CB Payload Decoding for Hexadecimal with latitude/longi
   let payload = [
     {
       variable: "payload_raw",
-      value: "f868508065628110f4602402105074814c6e0cba16010104000000000100fe01fd6756bb09015a775a06CF35D66682595d",
+      value: "f555666777888999f4678901234567894c6e0cba16010104000000000100fe01fd6756bb09015a775a06CF35D66682595d",
     },
   ];
 
@@ -212,4 +212,3 @@ describe("Shall not be parsed", () => {
     expect(payload).toEqual([{ variable: "shallnotpass", value: "04096113950292" }]);
   });
 });
-// {"IMEI":"861562073745280","IMSI":"724068042540503","Model":"SN50V3-NB","mod":1,"battery":3.59,"signal":26,"time":"2000/01/01 00:54:34","DS18B20_Temp":-409.5,"digital_in":0,"interrupt":0,"interrupt_level":0,"adc1":2,"temperature":0.0,"humidity":0.0,"1":[0.0,0.0,1,-409.5,"2000/01/01 07:14:11"],"2":[0.0,0.0,1,-409.5,"2000/01/01 06:59:11"],"3":[0.0,0.0,2,-409.5,"2000/01/01 06:44:11"],"4":[0.0,0.0,1,-409.5,"2000/01/01 06:29:11"],"5":[0.0,0.0,2,-409.5,"2000/01/01 06:14:11"],"6":[0.0,0.0,1,-409.5,"2000/01/01 05:59:11"],"7":[0.0,0.0,1,-409.5,"2000/01/01 05:44:11"],"8":[0.0,0.0,1,-409.5,"2000/01/01 05:29:11"]}
