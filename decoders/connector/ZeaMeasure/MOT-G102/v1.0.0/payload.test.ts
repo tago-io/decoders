f025e413f96d218b0848db0277e83b3a81067d9f
zhangchao <zhangc0320@163.com>
2026-01-25 11:33:37 +0800

    fix test file name

---
+++ decoders/connector/ZeaMeasure/MOT-G102/v1.0.0/payload.test.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test, beforeEach } from "vitest";
+import { DataToSend } from "@tago-io/sdk/lib/types";
+
+import { decoderRun } from "../../../../../src/functions/decoder-run";
+
+const file_path = "decoders/connector/ZenMeasure/MOT-G102/v1.0.0/payload.ts" as const;; // e.g. decoders/connector/your-decoder/v1.0.0/payload.ts
+
+let payload: DataToSend[] = [];
+
+describe("Shall not be parsed", () => {
+  let payload = [
+    { variable: "shallnotpass", value: "04096113950292" },
+    { variable: "fport", value: 9 },
+  ];
+  payload = decoderRun(file_path, { payload });
+  test("Output Result", () => {
+    expect(Array.isArray(payload)).toBe(true);
+  });
+
+  test("Not parsed Result", () => {
+    expect(payload).toEqual([
+      { variable: "shallnotpass", value: "04096113950292" },
+      { variable: "fport", value: 9 },
+    ]);
+  });
+});
